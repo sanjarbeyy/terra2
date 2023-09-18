f3f34e70b1e8e0c3dc782bfbf33d17e227b959d8
sanjarbeyy <sanjarbey1991@mail.ru>
2023-09-18 18:59:04 -0400

    newupdate

--- main.tf
+++ main.tf
@@ -31,7 +31,8 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
+  #instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
