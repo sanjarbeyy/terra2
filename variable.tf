0aa7f38740a586eb732606a4d31e15c36666c0a1
sanjarbeyy <sanjarbey1991@mail.ru>
2023-09-18 18:42:31 -0400

    updated

--- variable.tf
+++ variable.tf
@@ -1,7 +1,31 @@
+variable "region" {
+
+    description = "Region"
+
+    type = string
+
+    default = "us-east-1"
+
+ 
+
+}
+
 variable "instance_type" {
 
-  type=string
+  description = "The type of instance to start"
 
-  default = "t3.micro"
+  type        = string
+
+  default     = "t3.micro"
+
+}
+
+variable "ami_ssm_parameter" {
+
+  description = "SSM parameter name for the AMI ID. For Amazon Linux AMI SSM parameters see [reference](https://docs.aws.amazon.com/systems-manager/latest/userguide/parameter-store-public-parameters-ami.html)"
+
+  type        = string
+
+  default     = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
 
 }
